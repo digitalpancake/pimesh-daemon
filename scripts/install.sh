2dab389afa8e6f7458769a9b43a3e35e128aeba7
Jules White <juleswhite@127.0.0.1>
2024-07-12 21:11:57 +0100

    Add basic gum install and display welcome

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,14 @@
 # PiMesh
 # Gum?
 
+# Find architechture somehow
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
 if [[ -f "/lib/systemd/system/pimesh.service" ]]; then
 	echo "pimesh is already installed on your machine!"
 else
@@ -17,5 +25,18 @@ else
         sudo apt-get install git -y
     fi
 
+    if [ -d "/tmp/pmdmn" ]; then
+        rm -r /tmp/pmdmn/*
+    fi
+
+    mkdir /tmp/pmdmn
+    cd /tmp/pmdmn
+
     git clone https://github.com/digitalpancake/pimesh-daemon.git
+    wget https://github.com/charmbracelet/gum/releases/download/v0.14.1/gum_0.14.1_amd64.deb
+
+    apt install -y ./gum_0.14.1_amd64.deb
+
+    clear
+    gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Welcome to the PiMesh Daemon"
 fi
